713fd0c91d92c4e15846a8928ba839db14f65bd3
احمد طارق النونو <ahmed421137886@gmail.com>
2023-07-31 11:20:52 +0300

    add notification and fix some bugs

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -1,8 +1,14 @@
+import { Inject } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { USER_MODEL } from 'constants/constants';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { User } from 'src/models/user.model';
 
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
+  constructor(
+    @Inject(USER_MODEL)
+    private user: typeof User,
+  ) {
     super({
       clientID:
         '946583904276-l9spf0t8kcl5mmv8c94uv72akbo3vma7.apps.googleusercontent.com',
@@ -27,6 +33,10 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       accessToken,
     };
     console.log(profile);
+    const CreateUser = await this.user.create({
+      emial: profile.email,
+      password: profile.password,
+    });
     done(null, user);
   }
 }
